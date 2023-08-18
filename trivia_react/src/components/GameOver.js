f90cf64df6bb0d3b70edc3c03a7d6a92a95a30eb
Konstantinos Drosos <dean.drossos@gmail.com>
2023-08-18 15:45:21 -0400

    preparing site for deployment

--- trivia_react/src/components/GameOver.js
+++ trivia_react/src/components/GameOver.js
@@ -14,7 +14,8 @@ function GameOver(props){
             <div className="social_container">
                 <a href="https://www.facebook.com/sharer/sharer.php?kid_directed_site=0&sdk=joey&u=https%3A%2F%2Fraqueldrosos.com%2F&display=popup&ref=plugin&src=share_button" 
                 className="fa fa-facebook"></a>
-                <a href="https://www.instagram.com/raqueldrosos/" className="fa fa-instagram"></a>
+                <a href="https://www.instagram.com/raqueldrosos/" 
+                className="fa fa-instagram"></a>
             </div>
         </div>
     );
